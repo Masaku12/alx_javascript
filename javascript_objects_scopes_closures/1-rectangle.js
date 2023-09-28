c010c009cdfc5860748c12cd4085d17f0469e125
Ambrose Matata <ambrose.matata11@gmail.com>
2023-09-28 13:36:25 +0300

    defines a class Rectangle

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a rectangle
+ * @class
+ */
+
+class Rectangele {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+    }
+}
+
+module.exports = Rectangle;
