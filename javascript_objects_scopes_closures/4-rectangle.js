5e09b624d22254c9ed7be5ea616fa1c91a40ec17
Ambrose Matata <ambrose.matata11@gmail.com>
2023-09-28 21:47:28 +0300

    defines a rectangle with double & rotate features

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+/**
+ * Still defines a rectangle
+ * @class
+ */
+
+class Rectangle {
+    constructor(w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            let row = ''
+            for (let j = 0; j < this.width; j++) {
+                row += 'X'
+            }
+            console.log(row)
+        }
+    }
+
+    rotate() {
+        // Exchange width and height of the rectangle
+        [this.width, this.height] = [this.height, this.width]
+    }
+
+    double() {
+        // Double the width and height of the rectangle
+        this.width *= 2;
+        this.height *= 2;
+    }
+}
+
+module.exports = Rectangle;
