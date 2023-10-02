1e07056c9de994529d66952e98008e0e23080786
Ambrose Matata <ambrose.matata11@gmail.com>
2023-10-03 00:55:49 +0300

    script: displays status code of a GET request

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// A script that display the status code of a GET request
+
+// Getting the URL from the CLI
+const url = process.argv[2];
+
+// Importing the request module to make HTTP requests
+const request = require('request')
+
+// Send the GET request to the intended URL
+request.get(url, (error, response) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('code: ' + response.statusCode);
+    }
+});
