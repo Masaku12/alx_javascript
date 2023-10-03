fc5e759390c302d7084ae48de86813c3f6932d22
Ambrose Matata <ambrose.matata11@gmail.com>
2023-10-03 10:32:50 +0300

    script: prints the number of movies with Wedge

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+
+// Importing the request module to make HTTP requests
+const request = require('request');
+
+// Checks if the API URL is present
+if (process.argv.length !== 3) {
+    console.log("Usage: node 2-starwars_counts.js <API URL>"); 
+    process.exit(1);
+}
+
+// Gets the API URL from the CLI arg
+const apiUrl = process.argv[2];
+
+// Sends a GET request to the Star Wars API films endpoint
+request.get(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error("An error occured:", error);
+    } else if (response.statusCode !== 200) {
+        console.error(`Request failed with status code ${response.statusCode}`);
+    } else {
+        try {
+            // Parse the JSON response
+            const filmsData = JSON.parse(body);
+
+            // Initialize count for movies where Wedge Antilles is present
+            let count = 0;
+
+            // Loop through each film
+            filmsData.results.forEach((film) => {
+                // Check if character ID 18 is in the characters array
+                if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+                    count++;
+                }
+            });
+
+            // Print count of movies where Wedge Antilles is present
+            console.log(`${count}`);
+        } catch (parseError) {
+            console.error("Error parsing JSON response:", parseError);
+        }
+    }
+});
