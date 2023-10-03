ffae8b57d4ec5a1bbde682a7dbca1b8d2c912c1f
Ambrose Matata <ambrose.matata11@gmail.com>
2023-10-03 19:14:49 +0300

    fix: updated script

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -7,19 +7,4 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-// Sends a GET request to the given URL
-request.get(url, (error, response, body) => {
-    if (error) {
-        console.error("An error occured:", error);
-    } else if (response.statusCode !== 200) {
-        console.error(`Request failed with status code ${response.statusCode}`);
-    } else {
-        try {
-            // Writes the body response to the given URL
-            fs.writeFileSync(filePath, body, 'utf-8');
-            console.log(`${filePath}`);
-        } catch (writeError) {
-            console.error("Error writing to the file:", writeError);
-        }
-    }
-});
+request(url).pipe(fs.createWriteStream(filePath));
