056effc9cf2a15025b8d678510137e8110cb9cac
Ambrose Matata <ambrose.matata11@gmail.com>
2023-10-10 00:31:03 +0300

    Closure

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,35 @@
+// Creates a global variable
+let globalVariable = "Welcome";
+
+// Defines an outer function
+function outer() {
+    // Alerts the contents of the globalVariable
+    alert(globalVariable);
+
+    // Creates a variable with a value
+    let course = "Holberton";
+
+    // Defines an inner function
+    function inner() {
+        // Alerts the content of globalVariable & course
+        alert(globalVariable + " " + course);
+
+        // Creates a new variable
+        let exclamation = "!";
+
+        // Defines the inceptions function
+        function inception() {
+            // Alerts the contents of globalVariable, course, and exclamation
+            alert(globalVariable + " " + course + exclamation);
+        }
+
+        // Calls the Inception function
+        inception();
+    }
+
+    // Calls the inner function
+    inner();
+}
+
+// Calls the outer function
+outer();
